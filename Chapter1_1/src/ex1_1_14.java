2b44df9e06aa475ee1fc40ede6a062a4b1878827
noviceCoder0324 <88302674+noviceCoder0324@users.noreply.github.com>
2023-01-05 19:45:50 -0600

    1_1_14

--- Chapter1_1/src/ex1_1_14.java
+++ Chapter1_1/src/ex1_1_14.java
@@ -1,5 +1,44 @@
-public class ex1_1_14 {
-    public static void main(String[] args) {
+import edu.princeton.cs.algs4.StdOut;
 
+public class ex1_1_14 {
+
+    /**
+     * find the log 2 of N
+     *
+     * @param N
+     * @return
+     */
+    public static int lg(int N) {
+        int product = 1;
+        int i = 0;
+        while (product < N) {
+            product *= 2;
+            i++;
+        }
+        return i - 1;
     }
+
+    public static void main(String[] args) {
+        int n = 100;
+        int res = lg2(n);
+        StdOut.print(res);
+    }
+
+
+    /**
+     * This is a smarter way to do the same thing.
+     *
+     * @param N
+     * @return
+     */
+    public static int lg2(int N) {
+
+        int ret = 0;
+        while (N > 0) {
+            N /= 2;
+            ret++;
+        }
+        return ret - 1;
+    }
+
 }
