4ae365a690d3fcc9a2bc5c44d99c2036765420c0
noviceCoder0324 <88302674+noviceCoder0324@users.noreply.github.com>
2023-01-06 20:16:04 -0600

    21

--- Chapter1_1/src/ex1_1_21.java
+++ Chapter1_1/src/ex1_1_21.java
@@ -1,5 +1,38 @@
-public class ex1_1_21 {
-    public static void main(String[] args) {
+import edu.princeton.cs.algs4.StdIn;
 
+public class ex1_1_21 {
+
+    /**
+     * I never know this could be done like this.
+     */
+    public static class Record {
+        String name;
+        int v1;
+        int v2;
+
+        public Record(String name, int v1, int v2) {
+            this.name = name;
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        @Override
+        public String toString() {
+            return "Record {" + "name=" + name + ", v1=" + v1 + ", v2=" + v2 + "}";
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        while (!StdIn.isEmpty()) {
+            String line = StdIn.readLine();
+            if ("Q".equals(line)) {
+                return;
+            }
+            String[] rec = line.split(" ");
+            Record r = new Record(rec[0], Integer.parseInt(rec[1]), Integer.parseInt(rec[2]));
+            System.out.printf("%10s, %d, %d, %.3f%n", r.name, r.v1, r.v2, (double) r.v1 / r.v2);
+        }
     }
 }
